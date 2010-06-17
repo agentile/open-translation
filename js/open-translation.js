209b6a0e02421629a9e7b87087844a7f7c3ff305
Anthony <anthony@anthony-laptop.(none)>
2010-06-16 21:34:38 -0500

    some initial JS work

--- js/open-translation.js
+++ js/open-translation.js
@@ -1,6 +1,48 @@
+var csrf_token = '';
+var selected = '';
+var locale_code = 'en_US';
 var ot =
 {
-    init : function() {
+    init : function(csrf, native_code) {
+        csrf_token = csrf;
+        if (native_code) {
+            locale_code = native_code;
+        }
+        $(document).mouseup(function() {
+            selected = getSelectedText();
+            if (selected != '') {
+                loadTranslateDialog(locale_code, selected);
+            }
+        });
 
+        function getSelectedText() {
+            if (window.getSelection) {
+                return window.getSelection();
+            } else if (document.selection) {
+                return document.selection.createRange().text;
+            }
+            return '';
+        }
+
+        function loadTranslateDialog(locale_code, selected) {
+           $.ajax({
+                url: '/lib/ajax.php',
+                type: 'GET',
+                data: {
+                    ajax_action: 'fetch_translation',
+                    locale_code: locale_code,
+                    text: selected,
+                    csrf_token: csrf_token
+                },
+                dataType: 'json',
+                success: function(result) {
+                    if (result.success) {
+                        // display
+                    } else {
+                        // show error
+                    }
+                }
+            });
+        }
     }
 }
--- lib/config.php
+++ lib/config.php
@@ -1,11 +1,15 @@
 <?php # vim:ts=4:sw=4:et:
 $config = array();
 
+$config['translations'] = array(
+    'native_locale' => 'en_US',
+);
+
 $config['database'] = array(
     'type' => 'mysql',
     'host' => 'localhost',
     'user' => 'username',
     'pass' => 'password',
-    'name' => 'db_name'
+    'name' => 'db_name',
 );
 return $config;
