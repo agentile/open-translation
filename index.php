644fb0b052685bb266f189d62237b27b0a55f53c
Brian McAllister <brian@brianmcallister.com>
2011-10-13 14:49:32 -0400

    Add submission label styles.

--- index.php
+++ index.php
@@ -41,6 +41,23 @@ $locale = new OT_Locale(array('code' => $locale_code, 'path' => dirname(__FILE__
   <input type="text" class="ot-add-translation" />
   <blockquote class="ot-translate-this">Translate this</blockquote>
   <a href="#" class="ot-submit ot-pill">Submit</a>
+  
+  <ul class="ot-submitted-translations-list">
+    <li class="ot-list-green">
+      Some text
+      <span>+3</span>
+    </li>
+    
+    <li class="ot-list-green">
+      More text
+      <span>+2</span>
+    </li>
+    
+    <li class="ot-list-red">
+      Extra stuff
+      <span>-3</span>
+    </li>
+  </ul>
 
 
 	<script type="text/javascript">
