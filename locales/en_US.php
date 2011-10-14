5d9292f2b2695cef988352ad3484122c8faa24ba
Brian McAllister <brian@brianmcallister.com>
2011-10-14 12:34:25 -0400

    Update demo page.

--- locales/en_US.php
+++ locales/en_US.php
@@ -1,9 +1,7 @@
 <?php
 return array(
-    'WELCOME' => 'Welcome to Open Translation',
-    'PROJECT_DESCRIPTION' => 'This is the description of the open-translation project.',
-    'INSTRUCTIONS_HEADER' => 'How to use Open-Translation',
-    'EXAMPLE_TEXT' => 'Example using \'data-translatable-id\' attribute class.',
-    'TRANSLATE_THIS' => 'Translate this',
-    'SUBMIT' => 'Submit',
+    'CLICK-HERE'          => 'Click here',
+    'WELCOME'             => 'Welcome',
+    'SAVE-YOUR-PROGRESS'  => 'Save your progress',
+    'MAKE-NEW-ENTRY'      => 'Make new entry'
 );
