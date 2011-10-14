2dbf9ddcc2305d159d8ca61dcc3d726bb0ee42f9
Nano <nano.anderson@gmail.com>
2011-10-14 11:38:32 -0400

    fetching by id needed to return fetchOne() instead of fetch()
    Signed-off-by: Nano <nano.anderson@gmail.com>

--- ot/lib/db.php
+++ ot/lib/db.php
@@ -1045,7 +1045,7 @@ class OT_DB
         $data = array(
             'tid' => $tid,
         );
-        return $this->fetch($sql, $data);
+        return $this->fetchOne($sql, $data);
     }
     
     /**
